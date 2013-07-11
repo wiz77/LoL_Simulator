115a42c704e378a833d431c45795798b042abce4
kang <kang@kang.(none)>
2013-07-11 17:24:07 +0900

    챔피언 클래스 추가

---
+++ lol_simulator/WebContent/Champion/Champion.java
@@ -0,0 +1,27 @@
+
+public class Champion {
+	double health;
+	double healthRegen;
+	double mana;
+	double manaRegen;
+	double armor;
+	double magicResist;
+	double dodge;
+	double tenacity;
+	double movementSpeed;
+	double goldPer;
+	double attackDamage;
+	double attackSpeed;
+	double critChance;
+	double critDamage;
+	double abilityPower;
+	double lifeSteal;
+	double spellVamp;
+	double armorPen;
+	double magicPen;
+	double cooldownRed;
+	
+	Champion() {}
+	
+	
+}
